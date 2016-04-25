596e345537725d04fca9b6f05566a3e5c7a71e82
Frank Beckwith <fbeckwit@ucsd.edu>
2016-04-24 17:05:04 -0700

    Changed Node constructor and added boundary enum.
    Enumeration for node indicates whether it is an interior node, or on the
    essential or natural boundary.

--- Node.h
+++ Node.h
@@ -24,12 +24,18 @@ class Node {
 
 public:
 
-  /* ****************************  COPY CONTROL  **************************** */
-  /* Default constructor */
-  Node( ) : coord{ 0 }, disp{ 0 }
-  { }
+  /* ****************************  ENUMERATIONS  **************************** */
 
-  Node( double coord ) : coord{ coord }, disp{ 0 }
+  /* Enumeration to indicate if node is interior, or if on natural or essential
+   * boundary.
+   */
+  enum node_type { INT, EBC, NBC };
+
+  /* ****************************  COPY CONTROL  **************************** */
+
+  /* Default constructor */
+  Node( double coord = 0, node_type type = INT, double bc = 0 ) :
+    coord{ coord }, type{ type }, disp{ 0 }, bound_cond{ bc }
   { }
 
   /* Destructor */
@@ -42,8 +48,10 @@ private:
 
   /* ************************  PRIVATE DATA MEMBERS  ************************ */
 
-  double coord;     // Coordinate of the node;
-  double disp;      // Current displacement of the node;
+  double coord;       // Coordinate of the node;
+  node_type type;     // Marker indicating interior or boundary;
+  double disp;        // Current displacement of the node;
+  double bound_cond;  // Boundary condition if node is not interior;
 
   /* **********************  PRIVATE MEMBER FUNCTIONS  ********************** */
 
