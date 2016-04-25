2bd3a07e4f3cc9f2db6fac78d90c00e7527f859d
Frank Beckwith <fbeckwit@ucsd.edu>
2016-04-24 23:57:21 -0700

    Created 'Material' class and implemented basic functions

---
+++ Material.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** *
+ *                           Frank Nathan Beckwith                            *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ * ************************************************************************** *
+ *                                                                            *
+ * Source file for the implementation of the Material abstraction.            *
+ * Class definition given in Material.h.                                      *
+ *                                                                            *
+ * ************************************************************************** */
+
+// Project-specific headers;
+#include "Material.h"
+
+// System headers;
+
+/* *****************************  COPY CONTROL  ***************************** */
+
+/* ***********************  PUBLIC MEMBER FUNCTIONS  ************************ */
+
+/* get_tangent( )
+ * Return the tangent elastic modulii tensor for the material.
+ */
+Eigen::Matrix2d Material::get_tangent( ) const
+{
+  // Create the tangent matrix, load the entries and return;
+  Eigen::Matrix2d elastic_mod;
+  elastic_mod( 0, 0 ) = elastic_mod( 1, 1 ) = lambda + 2 * mu;
+  elastic_mod( 0, 1 ) = elastic_mod( 1, 0 ) = lambda;
+  return elastic_mod;
+}
+
+/* ***********************  PRIVATE MEMBER FUNCTIONS  *********************** */
---
+++ Material.h
@@ -0,0 +1,60 @@
+/* ************************************************************************** *
+ *                           Frank Nathan Beckwith                            *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ * ************************************************************************** *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ * ************************************************************************** */
+
+#ifndef GUARD_MATERIAL_H
+#define GUARD_MATERIAL_H
+
+// Project-specific headers;
+
+// System headers;
+#include <Eigen/LU>
+
+class Material {
+
+public:
+
+  /* ****************************  ENUMERATIONS  **************************** */
+
+  /* ****************************  COPY CONTROL  **************************** */
+
+  /* Default constructor */
+  Material( double E = 0, double nu = 0 )
+  {
+    lambda = nu * E / (( 1 + nu ) * ( 1 - 2 * nu ));
+    mu = E / 2 / ( 1 + nu );
+  }
+
+  Material * clone( ) const { return new Material( *this ); }
+
+  /* Destructor */
+
+  /* **********************  PUBLIC MEMBER FUNCTIONS  *********************** */
+
+  /* get_tangent( )
+   * Return the tangent elastic modulii tensor for the material.
+   */
+  Eigen::Matrix2d get_tangent( ) const;
+
+private:
+
+  /* ************************  PRIVATE DATA MEMBERS  ************************ */
+
+  double lambda;
+  double mu;
+
+  /* **********************  PRIVATE MEMBER FUNCTIONS  ********************** */
+
+};
+
+#endif
+
