abc166b8d8dfbf371b3e93dd25bbe840db7ef7b1
Frank Beckwith <fbeckwit@ucsd.edu>
2016-04-25 19:18:27 -0700

    Reworked 'main' to use Domain class

--- main.cpp
+++ main.cpp
@@ -19,51 +19,56 @@ void print_stiffness(
   out << e.get_stiffness( int_order ) << "\n]\n";
 }
 
+double calc_exact( double r, double P, double E, double nu )
+{
+  double a = 6.0;
+  double b = 9.0;
+  return P * a*a * r / E / (b*b - a*a) * ((1 + nu)*(1 - 2*nu) + b*b/(r*r) * (1 + nu));
+}
+
 int main( int argc, char *argv[] )
 {
 
-  std::size_t num_elem = 1;
   // Create some nodes;
-  std::vector<Node *> nodes;
-  std::vector<Element> elems;
-
   // Problem inputs (hard-coded for now);
   double a = 6.0;
   double b = 9.0;
   double P = 10.0;
   double E = 1000.0;
-  double nu = 0.49999;
+  double nu = 0.499;
 
+  // Load nodes;
+  Domain domain;
+  std::size_t num_elem = 80;
   double elem_size = ( b - a ) / num_elem;
   for( std::size_t node_i{ 0 }; node_i != num_elem + 1; ++node_i ) {
+    double coord = a + node_i * elem_size;
     if( node_i == 0 )
-      nodes.push_back( new Node( node_i,  a + node_i * elem_size, Node::NBC, 10.0 ) );
+      domain.create_node( coord, Node::NBC, P );
     else
-      nodes.push_back( new Node( node_i,  a + node_i * elem_size ));
+      domain.create_node( coord );
   }
 
-  Material mat1( E, nu );
-  std::cout << "mat1(" << &mat1 << ") = [\n" << mat1.get_tangent( ) << "\n]\n";
+  // Create material;
+  domain.create_material( E, nu );
 
-  for( std::size_t ele{ 0 }; ele != num_elem; ++ele )
-    elems.push_back( Element( ele, nodes[ ele ], nodes[ ele + 1 ], &mat1 ) );
+  for( std::size_t ele_i{ 0 }; ele_i != num_elem; ++ele_i ) {
+    domain.create_element( ele_i, ele_i + 1, 0 );
+  }
 
-  Eigen::MatrixXd stiff = elems[0].get_stiffness( 1 );
-  Eigen::VectorXd force = elems[0].get_force_ext( );
+  Eigen::VectorXd disp = domain.solve( );
 
-  Eigen::VectorXd disp = stiff.inverse( ) * force;
-  Eigen::VectorXd exact( 2 );
-  exact( 0 ) = P * a*a * a / E / (b*b - a*a) * ((1 + nu)*(1 - 2*nu) + b*b/(a*a)*(1 + nu));
-  exact( 1 ) = P * a*a * b / E / (b*b - a*a) * ((1 + nu)*(1 - 2*nu) + b*b/(b*b)*(1 + nu));
+  Eigen::VectorXd exact( num_elem + 1 );
+  for( std::size_t node_i{ 0 }; node_i != num_elem + 1; ++node_i ) {
+    double coord = a + node_i * elem_size;
+    exact( node_i ) = calc_exact( coord, P, E, nu );
+  }
 
   Eigen::VectorXd error = exact - disp;
 
   std::cout << "disp = [\n" << disp << "\n]\n";
   std::cout << "exact = [\n" << exact << "\n]\n";
   std::cout << "error = [\n" << error << "\n]\n";
-
-  for( std::size_t node_i{ 0 }; node_i != num_elem + 1; ++node_i )
-    delete nodes[node_i];
 
   return 0;
 }
