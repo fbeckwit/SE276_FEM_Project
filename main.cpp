dc0387f41a51319e4762b0f6a554a74223db4fa8
Frank Beckwith <fbeckwit@ucsd.edu>
2016-04-25 18:30:07 -0700

    Compared to exact

--- main.cpp
+++ main.cpp
@@ -27,8 +27,13 @@ int main( int argc, char *argv[] )
   std::vector<Node *> nodes;
   std::vector<Element> elems;
 
+  // Problem inputs (hard-coded for now);
   double a = 6.0;
   double b = 9.0;
+  double P = 10.0;
+  double E = 1000.0;
+  double nu = 0.49999;
+
   double elem_size = ( b - a ) / num_elem;
   for( std::size_t node_i{ 0 }; node_i != num_elem + 1; ++node_i ) {
     if( node_i == 0 )
@@ -37,7 +42,7 @@ int main( int argc, char *argv[] )
       nodes.push_back( new Node( node_i,  a + node_i * elem_size ));
   }
 
-  Material mat1( 1000, 0.4999999 );
+  Material mat1( E, nu );
   std::cout << "mat1(" << &mat1 << ") = [\n" << mat1.get_tangent( ) << "\n]\n";
 
   for( std::size_t ele{ 0 }; ele != num_elem; ++ele )
@@ -47,7 +52,15 @@ int main( int argc, char *argv[] )
   Eigen::VectorXd force = elems[0].get_force_ext( );
 
   Eigen::VectorXd disp = stiff.inverse( ) * force;
-  std::cout << disp << "\n";
+  Eigen::VectorXd exact( 2 );
+  exact( 0 ) = P * a*a * a / E / (b*b - a*a) * ((1 + nu)*(1 - 2*nu) + b*b/(a*a)*(1 + nu));
+  exact( 1 ) = P * a*a * b / E / (b*b - a*a) * ((1 + nu)*(1 - 2*nu) + b*b/(b*b)*(1 + nu));
+
+  Eigen::VectorXd error = exact - disp;
+
+  std::cout << "disp = [\n" << disp << "\n]\n";
+  std::cout << "exact = [\n" << exact << "\n]\n";
+  std::cout << "error = [\n" << error << "\n]\n";
 
   for( std::size_t node_i{ 0 }; node_i != num_elem + 1; ++node_i )
     delete nodes[node_i];
